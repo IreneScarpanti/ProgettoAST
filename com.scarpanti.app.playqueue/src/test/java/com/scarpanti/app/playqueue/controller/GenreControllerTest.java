dc62404f5b62dc19a90aceec104a80aa18ed4a30
IreneScarpanti <irenescarpanti@gmail.com>
2025-07-21 22:38:31 +0200

    Added Jacoco, PIT and Coveralls configurations

---
+++ com.scarpanti.app.playqueue/src/test/java/com/scarpanti/app/playqueue/controller/GenreControllerTest.java
@@ -0,0 +1,12 @@
+package com.scarpanti.app.playqueue.controller;
+
+import org.junit.Test;
+
+public class GenreControllerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
