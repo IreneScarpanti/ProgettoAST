aed5d1782354c7d344fb4702fdaccb32d276f400
IreneScarpanti <irenescarpanti@gmail.com>
2025-07-21 22:57:31 +0200

    Added GenreController using TransactionManager abstraction
    Including repository and view layers

---
+++ com.scarpanti.app.playqueue/src/main/java/com/scarpanti/app/playqueue/transaction/TransactionManager.java
@@ -0,0 +1,7 @@
+package com.scarpanti.app.playqueue.transaction;
+
+public interface TransactionManager {
+
+	<T> T doInTransaction(TransactionCode<?> any);
+
+}
---
+++ com.scarpanti.app.playqueue/src/main/java/com/scarpanti/app/playqueue/view/PlayQueueView.java
@@ -0,0 +1,11 @@
+package com.scarpanti.app.playqueue.view;
+
+import java.util.List;
+
+import com.scarpanti.app.playqueue.model.Genre;
+
+public interface PlayQueueView {
+
+	void showGenres(List<Genre> genres);
+
+}
