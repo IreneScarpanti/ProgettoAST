70473f0daac1443987a36bbf3203bd73c411dbea
IreneScarpanti <irenescarpanti@gmail.com>
2025-07-25 11:20:17 +0200

    Pom configuration with build-helper and failsafe for IT

---
+++ com.scarpanti.app.playqueue/src/it/java/com/scarpanti/app/playqueue/controller/ControllerIT.java
@@ -0,0 +1,14 @@
+package com.scarpanti.app.playqueue.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerIT {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
