9c169bb997a1a78064b7b4087ccab77e61ea3049
IreneScarpanti <irenescarpanti@gmail.com>
2025-07-26 14:22:07 +0200

    Refactored tests removing JPA properties redundancy

--- com.scarpanti.app.playqueue/src/test/java/com/scarpanti/app/playqueue/repository/jpa/JpaGenreRepositoryTest.java
+++ com.scarpanti.app.playqueue/src/test/java/com/scarpanti/app/playqueue/repository/jpa/JpaGenreRepositoryTest.java
@@ -41,11 +41,6 @@ public class JpaGenreRepositoryTest {
 		properties.put("javax.persistence.jdbc.url", mysql.getJdbcUrl());
 		properties.put("javax.persistence.jdbc.user", mysql.getUsername());
 		properties.put("javax.persistence.jdbc.password", mysql.getPassword());
-		properties.put("javax.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-		properties.put("hibernate.hbm2ddl.auto", "create-drop");
-		properties.put("hibernate.show_sql", "true");
-
 		emf = Persistence.createEntityManagerFactory("playqueue-test", properties);
 		em = emf.createEntityManager();
 		genreRepository = new JpaGenreRepository(em);
--- com.scarpanti.app.playqueue/src/test/java/com/scarpanti/app/playqueue/repository/jpa/JpaPlayQueueRepositoryTest.java
+++ com.scarpanti.app.playqueue/src/test/java/com/scarpanti/app/playqueue/repository/jpa/JpaPlayQueueRepositoryTest.java
@@ -53,9 +53,6 @@ public class JpaPlayQueueRepositoryTest {
 		properties.put("javax.persistence.jdbc.user", mysql.getUsername());
 		properties.put("javax.persistence.jdbc.password", mysql.getPassword());
 		properties.put("javax.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-		properties.put("hibernate.hbm2ddl.auto", "create-drop");
-		properties.put("hibernate.show_sql", "true");
 
 		emf = Persistence.createEntityManagerFactory("playqueue-test", properties);
 		em = emf.createEntityManager();
